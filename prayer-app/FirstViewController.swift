56e8acefe95bc066feef5141f286c528236348d4
Kyle Johannsen <kylejohannsen@kyles-macbook-air.local>
2019-01-03 21:12:20 -0500

    Initial Commit

---
+++ prayer-app/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  prayer-app
+//
+//  Created by Kyle Johannsen on 1/3/19.
+//  Copyright © 2019 Kyle Johannsen. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
